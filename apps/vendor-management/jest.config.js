b8d665c9a1d273bb64224f4c1644df73426728e3
Monda <levilexkilobytes@gmail.com>
2026-09-21 13:58:41 +0300

    test(vendor): add smoke test so tests/ is tracked, relax jest roots

--- apps/vendor-management/jest.config.js
+++ apps/vendor-management/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  roots: ['<rootDir>/src', '<rootDir>'],
   testMatch: ['**/*.test.ts', '**/*.spec.ts'],
   passWithNoTests: true,
   moduleNameMapper: {
